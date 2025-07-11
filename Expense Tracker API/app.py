08892cc5a079c03f9cbaab4d8e78e1c943559b01
AmanDevelops <amanpal2072004@gmail.com>
2025-07-11 10:23:50 +0530

    feat: add JWT authentication support and improve user registration error handling

--- Expense Tracker API/app.py
+++ Expense Tracker API/app.py
@@ -1,4 +1,8 @@
+import os
+import time
+
 import bcrypt
+import jwt
 from flask import Flask, request
 
 from models import Category, Expense, Session, User
@@ -6,6 +10,10 @@ from validators import validate_register
 
 app = Flask(__name__)
 import exceptions
+
+jwt_secret = os.environ.get("JWT_SECRET")
+if jwt_secret is None:
+    raise RuntimeError("Please setup the JWT secret key to the environment variables.")
 
 
 @app.route("/")
@@ -22,7 +30,7 @@ def register():
         user_data = session.query(User).filter_by(username=data["username"]).first()
 
         if user_data:
-            raise ValueError(f"Username {data['username']} already exists")
+            raise ValueError(f"Username '{data['username']}' already exists")
 
         new_user = User(
             username=data["username"],
@@ -33,3 +41,27 @@ def register():
         session.commit()
 
         return {"message": "User created succesfully!"}, 201
+
+
+@app.route("/auth/login", methods=["POST"])
+def login():
+    data = request.get_json()
+    validate_register(data)
+
+    with Session() as session:
+        user_data = session.query(User).filter_by(username=data["username"]).first()
+
+        if not user_data:
+            raise ValueError("Either username or passsword is incorrect")
+
+
+        if bcrypt.checkpw(data["password"].encode("utf-8"), user_data.password):
+            jwt_token = jwt.encode(
+                {"sub": user_data.id, "exp": int(time.time()) + 3600},
+                os.environ.get("JWT_SECRET"),
+                algorithm="HS256",
+            )
+
+            return {"message": "User authnticated succesfully", "token": jwt_token}, 200
+        
+    raise ValueError("Either username or passsword is incorrect")
