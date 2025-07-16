f3afe5aa2015b8dbc42a2f3ae8f4042b6e7b616f
AmanDevelops <amanpal2072004@gmail.com>
2025-07-16 20:11:53 +0545

    feat: enhance URL shortening service with Redis caching and database setup

--- URL Shortening Service/app.py
+++ URL Shortening Service/app.py
@@ -1,15 +1,39 @@
-from flask import Flask, Response
+from concurrent.futures import ThreadPoolExecutor
+
+from flask import Flask, Response, g
+from redis import ConnectionPool, Redis
 
 from auth.routes import auth_bp
+from config import *
 from database import Session, init_db
 from exceptions import AppException
 from exceptions.custom_exceptions import UrlNotFoundError
 from service.models import Urls
 from service.routes import url_bp
 
+executor = ThreadPoolExecutor(max_workers=10)
+
+
 init_db()
 
 app = Flask(__name__)
+
+redis_pool = ConnectionPool(
+    host=REDIS_HOST,
+    port=REDIS_PORT,
+    password=REDIS_PASSWORD,
+    db=0,
+    max_connections=20,
+    socket_connect_timeout=5,
+    socket_timeout=5,
+    retry_on_timeout=True,
+)
+
+
+@app.before_request
+def setup_database():
+    """Setup the database"""
+    g.r = Redis(connection_pool=redis_pool)
 
 
 @app.errorhandler(AppException)
@@ -24,13 +48,30 @@ app.register_blueprint(url_bp)
 
 @app.route("/<string:short_code>", methods=["GET"])
 def get_url_info(short_code):
+    cached_url = g.r.get(short_code)
+    if cached_url:
+        response_data = Response(None, 302, {"Location": cached_url.decode()})
+        executor.submit(update_views, short_code)
+        return response_data
+
     with Session() as session:
         db_url_data = session.query(Urls).filter_by(short_code=short_code).first()
         if not db_url_data:
             raise UrlNotFoundError
 
+        g.r.set(short_code, db_url_data.url, ex=10)
         db_url_data.views += 1
-        session.commit()
+        executor.submit(update_views, short_code)
 
         response_data = Response(None, 302, {"Location": db_url_data.url})
         return response_data
+
+
+def update_views(short_code):
+    with Session() as thread_session:
+        db_url_data = (
+            thread_session.query(Urls).filter_by(short_code=short_code).first()
+        )
+        if db_url_data:
+            db_url_data.views += 1
+            thread_session.commit()
--- URL Shortening Service/config.py
+++ URL Shortening Service/config.py
@@ -5,3 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 JWT_SECRET = os.environ.get("JWT_SECRET")
+REDIS_HOST = os.environ.get("REDIS_HOST")
+REDIS_PORT = int(os.environ.get("REDIS_PORT"))
+REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD")
